f96bd83af172742fad32ac13d9d033ea4ad65088
Rodrigo Balazs <rodrigobalazs@gmail.com>
2024-10-29 13:04:06 -0300

    adds project initial folder structure + updates pom.xml with Spring Security dependencies

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# spring-security
+# springboot-security
---
+++ src/test/java/com/rbalazs/securityapi/SecurityApiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.rbalazs.securityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
